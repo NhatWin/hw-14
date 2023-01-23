c41cd9acb993f47da06cb49ee7a28fab08992d8c
NhatWin <nhat0933@gmail.com>
2023-01-22 23:42:22 -0600

    Set up front end

--- controllers/routes.js
+++ controllers/routes.js
@@ -4,4 +4,12 @@ router.get("/", async (_, res) => {
   res.render("homepage");
 });
 
+router.get("/dashboard", async (_, res) => {
+  res.render("dashboard");
+});
+
+router.get("/login", async (_, res) => {
+  res.render("login");
+});
+
 module.exports = router;
