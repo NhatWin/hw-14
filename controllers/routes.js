2e98dbfb782f74761424d544d81e401e9222b870
NhatWin <nhat0933@gmail.com>
2023-01-30 21:59:57 -0600

    Work on login route

--- controllers/routes.js
+++ controllers/routes.js
@@ -2,8 +2,14 @@ const router = require("express").Router();
 const withAuth = require("../utils/authorize");
 const User = require("../models/Users.js");
 
-router.get("/", withAuth, async (_, res) => {
-  res.render("homepage");
+router.get("/", withAuth, async (req, res) => {
+  try {
+    res.render("homepage", {
+      loggedIn: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get("/dashboard", withAuth, async (_, res) => {
@@ -23,31 +29,38 @@ router.post("/api/user/create", async (req, res) => {
     });
 });
 
-router.post("/api/user/login"),
-  async (req, res) => {
-    try {
-      const userData = await User.findOne({
-        where: { username: req.body.username },
-      });
+router.post("/login", async (req, res) => {
+  try {
+    const userData = await User.findOne({
+      where: { username: req.body.username },
+    });
 
-      if (!userData) {
-        res.status(400).json({ message: "user not found" });
-        return;
-      }
-      const validate = await userData.checkPassword(req.body.password);
-
-      if (!validate) {
-        res.status(400).json({ message: "incorrect password" });
-        return;
-      }
-      req.session.save(() => {
-        req.session.user_id = userData.id;
-        req.session.logged_in = true;
-        res.json({ user: userData, message: "logged in" });
-      });
-    } catch (err) {
-      res.status(400).json(err);
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: "Incorrect username or password, please try again" });
+      return;
     }
-  };
+
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: "Incorrect email or password, please try again" });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.json({ user: userData, message: "You are now logged in!" });
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+  // Logs an exisiting user in
+});
 
 module.exports = router;
--- public/script.js
+++ public/script.js
@@ -1,26 +1,24 @@
 const loginBtn = document.querySelector("#loginBtn");
 const signUpBtn = document.querySelector("#signUpBtn");
 
-const loginHandle = async (event) => {
+loginBtn.addEventListener("click", (event) => {
   event.preventDefault();
-  const userName = document.querySelector("#username").value.trim();
-  const passWord = document.querySelector("#password").value.trim();
+  const userName = document.querySelector("#newUsername").value.trim();
+  const passWord = document.querySelector("#newPassword").value.trim();
   fetch("/api/user/login", {
     method: "POST",
-    body: { username: userName, password: passWord },
+    body: JSON.stringify({ username: userName, password: passWord }),
+    headers: { "Content-Type": "application/json" },
   });
-};
+});
 
-loginBtn.onclick = loginHandle;
-
-const signUpHandle = async (event) => {
+signUpBtn.addEventListener("click", (event) => {
   event.preventDefault();
   const userName = document.querySelector("#newUsername").value.trim();
   const passWord = document.querySelector("#newPassword").value.trim();
   fetch("/api/user/create", {
     method: "POST",
-    body: { username: userName, password: passWord },
+    body: JSON.stringify({ username: userName, password: passWord }),
+    headers: { "Content-Type": "application/json" },
   });
-};
-
-signUpBtn.onclick = signUpHandle;
+});
